a91ea4d6bf6c5dea309225940fe549bc0922f589
anishavagadiya <vagadiya11@gmail.com>
2023-01-10 21:52:11 +0000

    bestbuy anisha

---
+++ src/main/java/com/bestbuy/constants/EndPoints.java
@@ -0,0 +1,22 @@
+package com.bestbuy.constants;
+
+public class EndPoints {
+
+    //Product EndPoints
+    public static final String GET_ALL_PRODUCTS = "/products";
+    public static final String CREATE_PRODUCTS = "/products";
+    public static final String GET_SINGLE_PRODUCTS_BY_ID = "/products/{productID}";
+    public static final String UPDATE_PRODUCTS_BY_ID = "/products/{productID}";
+    public static final String DELETE_PRODUCTS_BY_ID = "/products/{productID}";
+
+    //Stores EndPoints
+    public static final String GET_ALL_STORES = "/stores";
+    public static final String CREATE_STORES = "/stores";
+    public static final String GET_SINGLE_STORE_BY_ID = "stores/{storeID}";
+    public static final String UPDATE_SINGLE_STORE_BY_ID = "stores/{storeID}";
+    public static final String DELETE_SINGLE_STORE_BY_ID = "stores/{storeID}";
+
+
+
+}
+
